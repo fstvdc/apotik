cef7daa870a41afaf240210daab33dc0a003f7f0
fstvdc <144410080+fstvdc@users.noreply.github.com>
2023-10-20 09:11:43 +0700

    Add files via upload

---
+++ apotik_friska/config.php
@@ -0,0 +1,7 @@
+<?php
+$host = 'localhost'; // Ganti sesuai dengan host MySQL Anda
+$username = 'root'; // Ganti dengan username database
+$password = ''; // Ganti dengan password database
+$database = 'apotik_friska'; // Ganti dengan nama database
+
+// Membuat koneksi ke database
